767fdcae62893338d544080ba57b5704f56ff0d9
GuiDicaprinus <gudicaprinus@gmail.com>
2021-11-25 22:42:47 -0300

    adicionando validação de login

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,15 +1,24 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
 import {DefaultResponseMsg} from "../../types/DefaultResponseMessage";
 import {LoginRequest} from '../../types/LoginRequest';
+import {UserModel} from "../../models/UserModel";
+import md5 from "md5";
+import {connectDb} from "../../middlewares/connectDb";
 
-export default (req : NextApiRequest, res : NextApiResponse<DefaultResponseMsg>) => {
+const userLogin = async (req : NextApiRequest, res : NextApiResponse<DefaultResponseMsg>) => {
     if(req.method === 'POST'){
         const body = req.body as LoginRequest;
-        if(body && body.login === 'admin@admin.com' && body.password === 'Admin@123'){
-            return res.status(200).json({msg: 'Autenticado com sucesso'});
+
+        if(!body || !body.login || !body.password){
+            res.status(400).json({error : 'Invalid user/password'})
         }
-        return res.status(400).json({msg: 'Usuário ou senha inválidos'});
+        const userFound = await UserModel.find({email : body.login, password : md5(body.password)})
+        if(userFound && userFound.length > 0){
+            return res.status(200).json({msg: 'Autenticated Successfuly'});
+        }
+        return res.status(400).json({msg: 'Invalid user/password'});
     }
-    return res.status(405).json({msg : 'Metodo inválido'});
+    return res.status(405).json({msg : 'Invalid method'});
 }
 
+export default connectDb(userLogin)
